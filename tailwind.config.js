6d219003382072b916bbfb4e95b63fc4bcc60d32
Trey Coggins <coggins.mp3@gmail.com>
2024-02-15 02:45:36 -0500

    restored config files

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @config {./tailwindcss.config.js} */
+module.exports = {
+  content: ["./views/**/*.{html,php,js}", "./src/**/*.{html,css,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "Helvetica", "Arial", "sans-serif"],
+      display: ["Orbitron"],
+    },
+    extend: {
+      colors: {
+        secondary: {
+          light: "#d7b87a",
+          dark: "#af8c4a",
+        },
+        primary: {
+          light: "#38785d",
+          DEFAULT: "#175442",
+          dark: "#063427",
+        },
+        bg: {
+          light: "#f1f5f9",
+          dark: "#cbd5e1",
+        },
+      },
+    },
+    screens: {
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+      tv: "1536px",
+    },
+  },
+  plugins: ["postcss-import"],
+};
