cf6ffd95718687251e406f097f0dacfbc8e56533
Trey Coggins <coggins.mp3@gmail.com>
2024-02-22 20:41:32 -0500

    made signup responsive

--- views/signin.view.php
+++ views/signin.view.php
@@ -2,23 +2,25 @@
 </head>
 
 <body class="bg-bg-light text-secondary-dark font-sans min-h-screen flex flex-col">
-<?php require("partials/_header.php") ?>
-<?php require("partials/_nav.php") ?>
-  <main class="mx-4 pt-4 pb-16 grow">
-    <h1 class="text-2xl py-4 ps-3">Welcome</h1>
-    <p class="text-lg mb-2">New to BitBuggy?</p>
-    <div class="mb-8">
-      <a href="/signup" class="w-7/12 p-1.5 rounded-md bg-secondary-dark text-primary text-xs">Create an Account</a>
+  <?php require("partials/_header.php") ?>
+  <?php require("partials/_nav.php") ?>
+  <main class="flex flex-col w-full mx-4 my-16 tablet:mx-auto tablet:max grow">
+    <h1 class="text-4xl tablet:mx-auto tablet:text-7xl desktop:text-9xl tablet:my-8">Welcome</h1>
+    <div class="flex flex-col tablet:order-3 tablet:items-center">
+      <p class="text-lg mb-2">New to BitBuggy?</p>
+      <div class="mb-8">
+        <a href="/signup" class="p-1.5 rounded-md bg-secondary-dark text-primary text-xs">Create an Account</a>
+      </div>
     </div>
-    <section class="border-secondary-dark border-2 w-10/12 m-auto p-2">
-      <small class="text-secondary-dark">Already a customer?</small>
-      <p class="shrink-text font-bold text-primary-dark m-0">Sign in.</p>
-      <form class="flex flex-col w-10/12 text-secondary-dark" action="/signin" method="POST">
+    <section class="flex flex-col justify-center border-secondary-dark border-2 w-10/12 mx-auto my-8 p-4 laptop:w-7/12 desktop:max-w-2xl laptop:p-10">
+      <small class="text-secondary-dark tablet:text-lg">Already a customer?</small>
+      <p class="font-bold text-primary m-0">Sign in.</p>
+      <form class="flex flex-col self-center my-10 w-11/12 text-secondary-dark" action="/signin" method="POST">
         <label for="user-verify" class="ms-4">Username</label>
-        <input name="user" type="text" class="m-2 rounded-md text-gray-700 border-secondary-dark border-2 p-1">
-        <label for="password-verify" class="ms-2">Password</label>
-        <input type="password" id="password-verify" name="password" class="m-2 rounded-md text-gray-700 border-secondary-dark border-2 p-1">
-        <button type="submit" name="submit-btn" class="bg-secondary-dark text-primary p-3 border-none m-4 rounded-md">Continue</button>
+        <input name="user" type="text" class="m-2 rounded-md text-gray-700 border-secondary-dark border-2 p-1.5">
+        <label for="password-verify" class="ms-4">Password</label>
+        <input type="password" id="password-verify" name="password" class="m-2 rounded-md text-gray-700 border-secondary-dark border-2 p-1.5">
+        <button type="submit" name="submit-btn" class="bg-secondary-dark text-primary p-5 border-none m-4 rounded-md">Continue</button>
       </form>
     </section>
   </main>
